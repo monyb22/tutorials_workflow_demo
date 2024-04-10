44e82e2e6346851fc41548ce61f28b8b09c57914
Caleb Brown <kravitz80@gmail.com>
2024-04-10 16:42:44 -0400

    first commit

---
+++ tutorials/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+
+from django.urls import reverse
+import pytest
+
+
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+
+
+from tutorials.models import Tutorial
+
+@pytest.fixture
+def test_create_tutorial():
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    assert tutorial.title == "Pytest"
+
+@pytest.fixture
+def another_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='More-Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_compare_tutorials(new_tutorial, another_tutorial):
+    assert new_tutorial.pk != another_tutorial.pk
